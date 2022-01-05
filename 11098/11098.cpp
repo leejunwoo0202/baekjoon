4ba863dc0221e3694b8c7ad8ee1f986af6d87474
leejunwoo0202 <gs04043@naver.com>
2022-01-05 23:26:22 +0900

    11098

---
+++ 11098/11098.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	int p = 0;
+	int c[100];
+	int maxCost = 0;
+	string name[100];
+	string max = "null";
+	string maxName[100];
+	    
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p;
+
+		for (int j = 0; j < p; j++)
+		{
+			cin >> c[j];			cin >> name[j];
+		}
+			for (int z = 0; z < p; z++)
+			{
+				for (int x = 1; x < p; x++)
+				{
+					if (c[z] < c[x] && maxCost < c[x])
+					{
+						maxCost = c[x];
+						max = name[x];
+					}
+					else if (c[z] > c[x] && maxCost < c[z])
+					{
+						maxCost = c[z];
+						max = name[z];
+					}
+				}
+				
+			}
+			maxCost = 0;
+			maxName[i] = max;
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << maxName[i] << endl;
+	}
+
+	return 0;
+}
---
+++ 9663/9663.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int answer = 0;
+int arr_size = 0;
+// 세로에 값이 존재하는지 확인하기 위한 행렬
+int* existing_site_arr;
+// 우측 대각선에 값이 존재하는지 확인하기 위한 행렬
+int* cross_right_up;
+// 좌측 대각선에 값이 존재하는지 확인하기 위한 행렬
+int* cross_left_up;
+
+void dfs(int depth)
+{
+	// if dfs depart end point, then stop;
+	if (depth == arr_size)
+	{
+		answer++;
+		return;
+	}
+
+	// go to the next level;
+	for (int i = 0; i < arr_size; i++)
+	{
+		if ((existing_site_arr[i] == 1) || (cross_right_up[depth + i] == 1) || (cross_left_up[arr_size - 1 + (depth - i)] == 1))
+			continue;
+
+		existing_site_arr[i] = cross_right_up[depth + i] = cross_left_up[arr_size - 1 + (depth - i)] = 1;
+		dfs(depth + 1);
+		existing_site_arr[i] = cross_right_up[depth + i] = cross_left_up[arr_size - 1 + (depth - i)] = 0;
+	}
+}
+
+int main()
+{
+	cin >> arr_size;
+
+	// 동적 할당을 이용하여, 이중 배열을 생성해준다.
+	existing_site_arr = (int*)malloc(sizeof(int) * arr_size);
+	cross_right_up = (int*)malloc(sizeof(int) * (2 * arr_size - 1));
+	cross_left_up = (int*)malloc(sizeof(int) * (2 * arr_size - 1));
+
+	dfs(0);
+
+	cout << answer;
+
+	return 0;
+}
